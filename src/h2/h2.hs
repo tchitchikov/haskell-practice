98770283fefe985b033acbba359eac2ecc1722d6
tchitchikov <jinterlan5@gmail.com>
2017-08-22 09:16:43 -0400

    added in continuously compounding interest using e

--- src/h2/h2.hs
+++ src/h2/h2.hs
@@ -1,9 +1,12 @@
 main = do
     print makeE
+    print fiveYear
 
 
 factorial x = product [1 .. x]
 inverseFactorial x = 1 / factorial x
 makeE = 1 + sum [inverseFactorial x | x <- [1 .. 12]]
 
-
+--  Principal(eʳᵃᵗᵉ * ᵗᵉʳᵐ)
+continuouslyCompoundingInterest princ rate term = princ * (makeE ** (rate * term))
+fiveYear = continuouslyCompoundingInterest 100 0.05 5
